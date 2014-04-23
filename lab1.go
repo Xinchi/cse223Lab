542331655a213c106f223735154444fa52bd534b
Max Gu <guxinchi2000@gmail.com>
2014-04-22 20:43:54 -0700

    done backend

--- lab1.go
+++ lab1.go
@@ -1,6 +1,7 @@
 package triblab
 
 import (
+	// "fmt"
 	"net"
 	"net/http"
 	"net/rpc"
@@ -13,10 +14,46 @@ func NewClient(addr string) trib.Storage {
 }
 
 // Serve as a backend based on the given configuration
+// func ServeBack(b *trib.BackConfig) error {
+// 	server := rpc.NewServer()
+// 	server.Register(b.Store)
+// 	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+// 	listener, e := net.Listen("tcp", b.Addr)
+// 	if e != nil {
+// 		if b.Ready != nil {
+// 			b.Ready <- false
+// 		}
+// 		return e
+// 	}
+// 	if b.Ready != nil {
+// 		b.Ready <- true
+// 	}
+// 	return http.Serve(listener, nil)
+// 	// return nil
+// }
+// func ServeBack(b *trib.BackConfig) error {
+// 	e := rpc.Register(b.Store)
+// 	if e != nil {
+// 		fmt.Println(e)
+// 		return e
+// 	}
+// 	rpc.HandleHTTP()
+// 	s, e := net.Listen("tcp", b.Addr)
+// 	if e != nil {
+// 		fmt.Println("server tcp error:", e)
+// 	} else {
+// 		go http.Serve(s, nil)
+// 		b.Ready <- true
+// 	}
+// 	return nil
+// }
+
+
+// Serve as a backend based on the given configuration
 func ServeBack(b *trib.BackConfig) error {
 	server := rpc.NewServer()
 	server.Register(b.Store)
-	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+	//	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
 	listener, e := net.Listen("tcp", b.Addr)
 	if e != nil {
 		if b.Ready != nil {
@@ -27,6 +64,5 @@ func ServeBack(b *trib.BackConfig) error {
 	if b.Ready != nil {
 		b.Ready <- true
 	}
-	return http.Serve(listener, nil)
-	// return nil
+	return http.Serve(listener, server)
 }
--- lab2.go
+++ lab2.go
@@ -5,11 +5,21 @@ import (
 )
 
 func NewBinClient(backs []string) trib.BinStorage {
-	panic("todo")
+	return &helper{Backs:backs}
 }
 
 func ServeKeeper(kc *trib.KeeperConfig) error {
-	panic("todo")
+	
+	cls := make([]trib.Storage, len(kc.Backs))
+	for index:= range kc.Backs{
+		cls[index] = NewClient(kc.Backs[index])
+	}
+	//set true
+	kc.Ready <- true
+
+	// keeper := Keeper()
+	// return keeper.Run()
+	return nil
 }
 
 func NewFront(s trib.BinStorage) trib.Server {
